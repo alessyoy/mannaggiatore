c9cef284bbd1f26509616bb2d89fb882c9289a85
Alessyoy <95503923+alessyoy@users.noreply.github.com>
2023-02-13 15:31:10 +0100

    Comment version

--- mannaggiatore.sh
+++ mannaggiatore.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Versione 1.0 13/02/2023
+
 LISTA_SANTI=$(curl -v --silent https://www.santodelgiorno.it/ 2>&1 | grep -o "\/san[a-z\/-]*" | sed -e 's/\///g' -e 's/-/ /g' -e 's/novena//g' -e "s/sant /sant\'/g" -e 's/\b\(.\)/\u\1/g' -e 's/ E / e /g' | egrep -vi "^santo$" | egrep -vi "^santod[a-zA-Z].{1,8}" | egrep -vi "^santuario$" | sort --unique)
 
 OUTFILE=/tmp/filesanto
