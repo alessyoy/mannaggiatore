4022d0814a29b20703346711885c71a3819440eb
Alessyoy <95503923+alessyoy@users.noreply.github.com>
2023-02-13 19:02:56 +0100

    prova n°1 getopts

--- mannaggiatore.sh
+++ mannaggiatore.sh
@@ -2,6 +2,24 @@
 
 # Versione 1.0 13/02/2023
 
+
+usage() { echo "Bestemmia con stile, un santo diverso ogni volta!" 1>&2 ; exit 1; }
+
+while getopts ":h:s:" o; do
+    case "${o}" in
+	h) usage
+	   exit 0
+	   ;;
+	s) echo "shutdown in corso..."
+	   exit 0
+	   ;;
+	\?) printf "Illegal option: -%s\n" "$OPTARG" >&2
+	   echo "Try -h" >&2
+	   exit 1
+	   ;;
+    esac
+done
+
 LISTA_SANTI=$(curl -v --silent https://www.santodelgiorno.it/ 2>&1 | grep -o "\/san[a-z\/-]*" | sed -e 's/\///g' -e 's/-/ /g' -e 's/novena//g' -e "s/sant /sant\'/g" -e 's/\b\(.\)/\u\1/g' -e 's/ E / e /g' | egrep -vi "^santo$" | egrep -vi "^santod[a-zA-Z].{1,8}" | egrep -vi "^santuario$" | sort --unique)
 
 OUTFILE=/tmp/filesanto
